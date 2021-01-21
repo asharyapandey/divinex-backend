1f40224fdd8458063cf5c50f2b97e162c863e890
asharyapandey <asharyap7@gmail.com>
2021-01-21 10:52:17 +0545

    added request validation and encrypted the password

--- routes/user.js
+++ routes/user.js
@@ -4,22 +4,41 @@ const Router = express.Router();
 
 // User MOdel
 const User = require("../models/User");
+const { check, validationResult } = require("express-validator");
 
-Router.post("/", async (req, res) => {
-	const { username, email, password } = req.body;
+const { hashPassword } = require("../utils/utils");
 
-	const user = User({
-		username,
-		email,
-		password,
-	});
-	try {
-		await user.save();
-		return res.json(user);
-	} catch (error) {
-		return res.status(400).json({ error: "invalid details" });
+Router.post(
+	"/",
+	[
+		check("username", "Please provide a username").not().isEmpty(),
+		check("email", "Please provide a valid email").isEmail(),
+		check("password", "Please provide a Password").not().isEmpty(),
+	],
+	async (req, res) => {
+		const errors = validationResult(req);
+
+		if (errors.isEmpty()) {
+			const { username, email, password } = req.body;
+
+			try {
+				const hashedPassword = await hashPassword(password);
+				const user = User({
+					username,
+					email,
+					password: hashedPassword,
+				});
+				await user.save();
+				return res.json(user);
+			} catch (error) {
+				console.log(error);
+				return res.status(400).json({ error: "invalid details" });
+			}
+		} else {
+			res.send(errors.array());
+		}
 	}
-});
+);
 
 Router.get("/:id", async (req, res) => {
 	const id = req.params.id;
