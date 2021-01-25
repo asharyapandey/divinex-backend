1e8a172f359476f224fc971943cb2482fa7185a1
asharyapandey <asharyap7@gmail.com>
2021-01-25 12:39:05 +0545

    user login done: provided jwt after login

--- routes/user.js
+++ routes/user.js
@@ -6,8 +6,9 @@ const Router = express.Router();
 const User = require("../models/User");
 const { check, validationResult } = require("express-validator");
 
-const { hashPassword } = require("../utils/utils");
+const { hashPassword, verifyPassword, createToken } = require("../utils/utils");
 
+// route will be used for registration
 Router.post(
 	"/",
 	[
@@ -42,12 +43,40 @@ Router.post(
 	}
 );
 
+// route will be used for login
+Router.get("/", async (req, res) => {
+	try {
+		const { username, password } = req.body;
+		const user = await User.findOne({ username });
+		if (user === null)
+			return res.status(400).json({ error: "User Not Found" });
+		const passwordVerification = await verifyPassword(
+			password,
+			user.password
+		);
+		if (passwordVerification) {
+			const jwt = createToken(user);
+			return res.status(200).json({
+				msg: "User Logged In",
+				token: jwt,
+			});
+		} else {
+			return res
+				.status(400)
+				.json({ error: "Either Username or Password is incorrects" });
+		}
+	} catch (error) {}
+});
+
 Router.get("/:id", async (req, res) => {
 	const id = req.params.id;
 	try {
 		const user = await User.findOne({ _id: id });
+		if (user === null) {
+			return res.status(201).json({ error: "User not Found" });
+		}
 
-		res.json(user);
+		return res.json(user);
 	} catch (error) {
 		return res.status(400).json({ error: "No User Found" });
 	}
