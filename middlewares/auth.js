88041c51c3b70a5597c3925c722f0f69fc35b9b4
asharyapandey <asharyap7@gmail.com>
2021-01-28 10:40:14 +0545

    added verifyUser middleware to verify user jwt token

---
+++ middlewares/auth.js
@@ -0,0 +1,18 @@
+const jwt = require("jsonwebtoken");
+
+const verifyUser = (req, res, next) => {
+	const token = req.header("auth-token");
+
+	if (!token) {
+		return res.status(401).json({ error: "error! token not found" });
+	}
+
+	try {
+		const decoded = jwt.verify(token, "random-secret");
+		// add user to the request
+		req.user = decoded;
+	} catch (error) {
+		console.log(error);
+		return res.status(400).json({ error: "invalid token" });
+	}
+};
